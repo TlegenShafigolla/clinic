14deed4078f67a25e6db6042e594c35599f3022f
Tlegen <shafigolla.tlegen@gmail.com>
2026-07-29 06:44:06 +0500

    feat: add specialization lookup endpoint

--- src/main/java/kz/tlegen/clinic/controller/SpecializationController.java
+++ src/main/java/kz/tlegen/clinic/controller/SpecializationController.java
@@ -5,12 +5,7 @@ import kz.tlegen.clinic.dto.specialization.SpecializationRequest;
 import kz.tlegen.clinic.dto.specialization.SpecializationResponse;
 import kz.tlegen.clinic.service.SpecializationService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +27,10 @@ public class SpecializationController {
     @GetMapping
     public List<SpecializationResponse> findAll() {
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public SpecializationResponse findById(@PathVariable Long id) {
+        return service.findById(id);
     }
 }
