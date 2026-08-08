fdb485bfdff8c85759b087472c4e0281cfc994b0
Tlegen <shafigolla.tlegen@gmail.com>
2026-08-08 05:41:31 +0500

    test: add specialization repository integration test

---
+++ src/test/java/kz/tlegen/clinic/repository/SpecializationRepositoryTest.java
@@ -0,0 +1,37 @@
+package kz.tlegen.clinic.repository;
+
+import kz.tlegen.clinic.entity.Specialization;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@DataJpaTest
+@Testcontainers
+public class SpecializationRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres =
+            new PostgreSQLContainer("postgres:16-alpine");
+
+    @Autowired
+    private SpecializationRepository repository;
+
+    @Test
+    void existsByName_shouldReturnTrue_whenSpecializationExists() {
+        Specialization specialization = new Specialization("Cardiology");
+        repository.save(specialization);
+        boolean exists =
+                repository.existsByName("Cardiology");
+
+        assertTrue(exists);
+
+    }
+}
