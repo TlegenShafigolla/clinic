4b8fc9b86b76ead1532f3c72aeab934f7020870e
Tlegen <shafigolla.tlegen@gmail.com>
2026-08-11 23:23:44 +0500

    feat: add doctor creation service

--- src/main/java/kz/tlegen/clinic/service/DoctorService.java
+++ src/main/java/kz/tlegen/clinic/service/DoctorService.java
@@ -1,23 +1,40 @@
 package kz.tlegen.clinic.service;
 
+import kz.tlegen.clinic.dto.doctor.DoctorRequest;
+import kz.tlegen.clinic.dto.doctor.DoctorResponse;
 import kz.tlegen.clinic.entity.Doctor;
+import kz.tlegen.clinic.entity.Specialization;
+import kz.tlegen.clinic.exception.SpecializationNotFoundException;
+import kz.tlegen.clinic.mapper.DoctorMapper;
 import kz.tlegen.clinic.repository.DoctorRepository;
+import kz.tlegen.clinic.repository.SpecializationRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DoctorService {
     private final DoctorRepository doctorRepository;
+    private final SpecializationRepository specializationRepository;
+    private final DoctorMapper mapper;
 
-    public DoctorService(DoctorRepository doctorRepository) {
+    public DoctorService(DoctorRepository doctorRepository, SpecializationRepository specializationRepository, DoctorMapper mapper) {
         this.doctorRepository = doctorRepository;
-    }
-    public List<Doctor> getAllDoctors() {
-        return doctorRepository.findAll();
+        this.specializationRepository = specializationRepository;
+        this.mapper = mapper;
     }
 
-    public Doctor createDoctor(Doctor doctor) {
-        return doctorRepository.save(doctor);
+    public DoctorResponse create(DoctorRequest request) {
+        Long specializationId = request.getSpecializationId();
+
+        Specialization specialization =
+                specializationRepository.findById(specializationId)
+                        .orElseThrow(
+                                () -> new SpecializationNotFoundException(
+                                        "Specialization not found with id: " + specializationId
+                                )
+                        );
+
+        Doctor doctor = mapper.toEntity(request, specialization);
+        Doctor savedDoctor = doctorRepository.save(doctor);
+        return mapper.toResponse(savedDoctor);
     }
 }
