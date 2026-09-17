b94188525744813e6fdc323f6d40123abed06d27
Tlegen <shafigolla.tlegen@gmail.com>
2026-09-17 22:12:54 +0500

    create role and entity for users

---
+++ src/main/java/kz/tlegen/clinic/entity/Role.java
@@ -0,0 +1,7 @@
+package kz.tlegen.clinic.entity;
+
+public enum Role {
+    ADMIN,
+    DOCTOR,
+    PATIENT
+}
---
+++ src/main/java/kz/tlegen/clinic/entity/User.java
@@ -0,0 +1,56 @@
+package kz.tlegen.clinic.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean active;
+
+
+    protected User() {
+    }
+
+    public User(String email, String password, Role role, boolean active) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+}
