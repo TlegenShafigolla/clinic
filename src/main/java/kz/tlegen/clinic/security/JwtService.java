f3345d8e41bc824643b34249fe6afda873e57c96
Tlegen <shafigolla.tlegen@gmail.com>
2026-09-21 20:23:24 +0500

    add jwt

---
+++ src/main/java/kz/tlegen/clinic/security/JwtService.java
@@ -0,0 +1,24 @@
+package kz.tlegen.clinic.security;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    private SecretKey getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
