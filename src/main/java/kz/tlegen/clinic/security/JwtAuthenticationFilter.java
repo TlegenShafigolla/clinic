d81cc5a5ce163fa48aad5c38a7dfcac8804f2c52
Tlegen <shafigolla.tlegen@gmail.com>
2026-09-22 12:13:52 +0500

    feat: integrate Jwt filter

---
+++ src/main/java/kz/tlegen/clinic/security/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package kz.tlegen.clinic.security;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kz.tlegen.clinic.entity.User;
+import kz.tlegen.clinic.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        String email;
+        try {
+            email = jwtService.extractEmail(token);
+        } catch (JwtException | IllegalArgumentException exception) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        User user = userRepository.findByEmail(email)
+                .orElse(null);
+
+        if (user == null || !jwtService.isTokenValid(token, user)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(
+                        "ROLE_" + user.getRole().name()
+                );
+
+        filterChain.doFilter(request, response);
+    }
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+}
